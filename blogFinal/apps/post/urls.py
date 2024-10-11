ff2a8c0fa844027825b9ad56ced95afb364890ec
RomiAlegre21 <rominalejandralegre@gmail.com>
2024-10-11 13:55:56 -0300

    cambios urls css

--- blogFinal/apps/post/urls.py
+++ blogFinal/apps/post/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path("actualizar_post/<int:pk>", ActualizarPost.as_view(), name='actualizar_post'),
     path("eliminar_post/<int:pk>", EliminarPost.as_view(), name='eliminar_post'),
     path("listar_post/", ListarPost.as_view(), name='listar_post'),
-    
+        
     path("agregar_categoria/", CrearCategoria.as_view(), name= 'agregar_categoria'),
     path("actualizar_categoria/<int:pk>", ActualizarCategoria.as_view(), name = 'actualizar_categoria'),
     path("eliminar_categoria/<int:pk>", EliminarCategoria.as_view(), name= 'eliminar_categoria'),
