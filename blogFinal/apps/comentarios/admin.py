e4b9e8dda44bc93c83506818f26001e848e6a6f9
Maurinho-Suarez <suarezmauricio20@gmail.com>
2024-10-19 21:18:53 -0300

    Configuracion de las apps en la cuenta de superusuario/admin

--- blogFinal/apps/comentarios/admin.py
+++ blogFinal/apps/comentarios/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Comentario
+
+
+
+@admin.register(Comentario)
+class ComentariosAdmin(admin.ModelAdmin):
+    list_display = ('user','post','texto','fecha')
--- blogFinal/apps/post/admin.py
+++ blogFinal/apps/post/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Categoria, Post
+
+
+
+@admin.register(Post)
+class PostsAdmin(admin.ModelAdmin):
+    list_display = ('titulo','autor','categoria','descripcion_post',
+                    'fecha_creado','fecha_editado','imagen')
+
+admin.site.register(Categoria)
