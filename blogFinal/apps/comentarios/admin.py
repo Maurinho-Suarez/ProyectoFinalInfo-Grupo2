8a5ae6f0a76a8369da7a0a719553f968c3b549cc
RomiAlegre21 <rominalejandralegre@gmail.com>
2024-10-21 22:13:35 -0300

    corrección admin.py

--- blogFinal/apps/comentarios/admin.py
+++ blogFinal/apps/comentarios/admin.py
@@ -2,9 +2,6 @@
 from django.contrib import admin
 from .models import Comentario
 
-
 @admin.register(Comentario)
 class ComentariosAdmin(admin.ModelAdmin):
-    list_display = ('user','post','texto','fecha')
-
-admin.site.register(Comentario)
+    list_display = ('user', 'post', 'texto', 'fecha')
